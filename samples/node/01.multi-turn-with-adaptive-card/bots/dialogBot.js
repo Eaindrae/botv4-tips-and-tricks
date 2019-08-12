35b0ce8d9a2c19d3c84d3bfc2433195cffe00ddb
Eunji Kim <eunk.angie@outlook.com>
2019-08-12 19:27:53 +0900

    update adaptive card sample

---
+++ samples/node/01.multi-turn-with-adaptive-card/bots/dialogBot.js
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler } = require('botbuilder');
+
+class DialogBot extends ActivityHandler {
+    /**
+     *
+     * @param {ConversationState} conversationState
+     * @param {UserState} userState
+     * @param {Dialog} dialog
+     */
+    constructor(conversationState, userState, dialog) {
+        super();
+        if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
+        if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
+        if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
+
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.dialogState = this.conversationState.createProperty('DialogState');
+
+        this.onMessage(async (context, next) => {
+            console.log('Running dialog with Message Activity.');
+
+            // Run the Dialog with the new message Activity.
+            await this.dialog.run(context, this.dialogState);
+
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            for (const idx in context.activity.membersAdded) {
+                if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
+                    await context.sendActivity(`Hello user! Please type anything to proceed.`);
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onDialog(async (context, next) => {
+            // Save any state changes. The load happened during the execution of the Dialog.
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+            await next();
+        });
+    }
+}
+
+module.exports.DialogBot = DialogBot;
---
+++ samples/node/01.multi-turn-with-adaptive-card/dialogs/getUserInfoDialog.js
@@ -0,0 +1,55 @@
+const { ActivityPrompt, ComponentDialog, DialogSet, DialogTurnStatus, WaterfallDialog } = require('botbuilder-dialogs')
+const { CardFactory, MessageFactory } = require('botbuilder')
+const sampleInputCard = require('../sampleCard.json');
+
+const USER_PROFILE = 'USER_PROFILE';
+const CARD_PROMPT = 'CARD_PROMPT';
+const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
+
+class GetUserInfoDialog extends ComponentDialog {
+    constructor(userState) {
+        super('getUserInfoDialog');
+
+        this.userProfile = userState.createProperty(USER_PROFILE);
+
+        this.addDialog(new ActivityPrompt(CARD_PROMPT, this.inputValidator));
+
+        this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
+            this.showCardStep.bind(this),
+            this.showUserInputStep.bind(this)
+        ]));
+
+        this.initialDialogId = WATERFALL_DIALOG;
+    }
+
+    async run(turnContext, accessor){
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
+
+        const dialogContext = await dialogSet.createContext(turnContext);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
+    }
+
+    async showCardStep(step){
+        const inputForm = MessageFactory.attachment(CardFactory.adaptiveCard(sampleInputCard));
+        return await step.prompt(CARD_PROMPT,{
+            prompt: inputForm
+        });
+    }
+
+    async showUserInputStep(step){
+        const userInput = step.result.value;
+        await step.context.sendActivity(`Hello ${userInput.myName}, your email address: ${userInput.myEmail} and phone number: ${userInput.myTel}`);
+        return await step.endDialog();
+    }
+    
+    async inputValidator(promptContext){
+        const userInputObject = promptContext.recognized.value;
+        return true;
+    }
+}
+
+module.exports.GetUserInfoDialog = GetUserInfoDialog;
